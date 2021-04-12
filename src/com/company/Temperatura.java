45c4a074d20b953d70ccb4234a56f0b1895225d3
Beata <beatahumeniuk@gmail.com>
2021-04-12 20:16:51 +0200

    Update Temperatura.java

--- src/com/company/Temperatura.java
+++ src/com/company/Temperatura.java
@@ -4,15 +4,17 @@ package com.company;
 
 public class Temperatura {
         public static void main(String... args) {
-
+//UWAGA: nazwy zmiennych i klas piszemy po angielsku
             int naPlusieTemperatura = 18;
             int naMinusieTemperatura = -30;
 
             isTemperaturePositive(naPlusieTemperatura);
             isTemperaturePositive(naMinusieTemperatura);
+//        czy temperatura jest zawsze liczbą całkowitą?
         }
-
+// co daje nam typ tej metody skoro nigdzie go nie wykorzystujemy?
         private static boolean isTemperaturePositive(int temperature) {
+//            to można wrzucić do ifa, nie jest to duże wyrażenie i nie zepsuje czytelności kodu
             boolean isPositive = temperature > 0;
 
             if (isPositive) {
